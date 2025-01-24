547b58b7bdf22adbc534e7c04ef8885e24dd6ef6
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-24 12:05:50 -0300

    Adiciona rota funcional para cadastro de usuarios

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { AppDataSource } from "./data-source"
 import { User } from "./entity/User"
 import express, { NextFunction, Request, Response } from "express"
 import cors from "cors"
+import routes_user from "./routes/user.routes"
 
 const server = express()
 server.use(cors())
 server.use(express.json())
 
+server.use("/users", routes_user)
 
 AppDataSource.initialize()
-.then(() => server.listen(5432, () => {console.log("Hawk tuah on port 5432!")}))
+.then(() => server.listen(5431, () => {console.log("Hawk tuah on port 5431!")}))
 .catch(() => {console.log("no hawk tuahs")})
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,31 @@
-import { Request, Response, Router } from "express";
+import { Request, Response, Router } from 'express';
 import { User } from "../entity/User";
 import { AppDataSource } from "../data-source";
 
 const userRepository = AppDataSource.getRepository(User)
 const routes_user = Router()
 
-routes_user.post("/cadastrarUsuario", async (req:Request, res:Response) => {
+routes_user.post("/cadastrar", async (req: Request, res: Response) => {
+    try {
+        const new_user = userRepository.create(
+            {
+                name: req.body.name,
+                email: req.body.email,
+                senha: req.body.senha
+            })
+
+
+                res.status(201).json(new_user)
+            } 
+
+        catch (error) 
+            {
+                res.status(404).json({message:"No hawk tuah"})
+            }
+})
+export default routes_user;
+
+/** routes_user.post("/cadastrarUsuario", async (req:Request, res:Response) => {
         try {
         const new_user = userRepository.create(
             {
@@ -22,4 +42,5 @@ routes_user.post("/cadastrarUsuario", async (req:Request, res:Response) => {
             {
                 return res.status(404).json({message:"No hawk tuah"})
             }
-    })
+})
+ */
