5522ab389f1c19ffb3fb0c56305b9bcb9697f284
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-27 09:38:12 -0300

    Feature: adiciona a rota para postar novo medicamentos Fix: corrige erros onde o codigo nao esperava para se conectar ao banco Feature: adiciona nova script no package json para não precisar reiniciar novos codigos

--- src/routes/medicamentos.routes.ts
+++ src/routes/medicamentos.routes.ts
@@ -10,7 +10,7 @@ const medicamentoRepository = AppDataSource.getRepository(Medicamento)
 Routes_Med.get('/all', async (req: Request, res: Response) => {
     try 
     {
-        const medicamentos = medicamentoRepository.find()
+        const medicamentos = await medicamentoRepository.find()
         if(Object.keys(medicamentos).length === 0)
             {
                 res.status(201).json('Não há medicamentos cadastrados!')
@@ -32,7 +32,7 @@ Routes_Med.get('/med/:id', async (req: Request, res:Response) =>
     try 
     {
         const body_id = req.params.id
-        const medicamento_procurado = medicamentoRepository.findOneBy({id: parseInt(body_id)}) 
+        const medicamento_procurado = await medicamentoRepository.findOneBy({id: parseInt(body_id)}) 
 
         if(Object.keys(medicamento_procurado).length === 0)
             {
@@ -49,6 +49,32 @@ Routes_Med.get('/med/:id', async (req: Request, res:Response) =>
         res.status(500).json('não foi possivel encontrar o servidor')
     }
 })
+
+Routes_Med.post('/cadastrarMedicamento', async (req:Request, res:Response) => 
+    {
+        try 
+        {
+            const medBody = req.body as Medicamento
+
+            if(!medBody || !medBody.description || !medBody.name || !medBody.user_id)
+                {
+                    res.status(400).json("Todos os campos precisam ser preenchidos!")
+                }
+            
+            await medicamentoRepository.save(medBody)
+            res.status(201).json(medBody)
+        } 
+
+
+
+        catch (error) 
+        {
+            res.status(500).json('não foi possivel encontrar o servidor')
+        }
+        
+
+        
+    })
 
 
 
