863c50047070c3db04d18361745f6c855f39073f
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-27 09:21:24 -0300

    Feature: Adiciona rota para pesquisar Medicamento especifico

--- src/routes/medicamentos.routes.ts
+++ src/routes/medicamentos.routes.ts
@@ -27,6 +27,29 @@ Routes_Med.get('/all', async (req: Request, res: Response) => {
         res.status(500).json('não foi possivel encontrar o servidor')
     }
 
+Routes_Med.get('/med/:id', async (req: Request, res:Response) =>  
+{
+    try 
+    {
+        const body_id = req.params.id
+        const medicamento_procurado = medicamentoRepository.findOneBy({id: parseInt(body_id)}) 
+
+        if(Object.keys(medicamento_procurado).length === 0)
+            {
+                res.status(201).json("o medicamento que voce procura não esta cadastrado")
+            }
+        else
+        {
+            res.status(201).json(medicamento_procurado)
+        }
+    } 
+
+    catch (error) 
+    {
+        res.status(500).json('não foi possivel encontrar o servidor')
+    }
+})
+
 
 
 })
