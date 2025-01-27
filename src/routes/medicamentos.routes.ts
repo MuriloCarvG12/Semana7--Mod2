75322cf2365d17f9733da670a9d002cae1390901
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-27 09:09:53 -0300

    Feature: adiciona rota para busca de todos os medicamentos

--- src/routes/medicamentos.routes.ts
+++ src/routes/medicamentos.routes.ts
@@ -1,4 +1,35 @@
 import {Request, Response, Router} from "express"
 import {AppDataSource} from "../data-source"
+import { Medicamento } from "../entity/Medicamento"
+
 
 const Routes_Med = Router()
+
+const medicamentoRepository = AppDataSource.getRepository(Medicamento)
+
+Routes_Med.get('/all', async (req: Request, res: Response) => {
+    try 
+    {
+        const medicamentos = medicamentoRepository.find()
+        if(Object.keys(medicamentos).length === 0)
+            {
+                res.status(201).json('Não há medicamentos cadastrados!')
+            }
+        else
+        {
+            res.status(201).json(medicamentos)
+        }
+        
+    } 
+
+    catch (error) 
+    {
+        res.status(500).json('não foi possivel encontrar o servidor')
+    }
+
+
+
+})
+
+
+export default Routes_Med
